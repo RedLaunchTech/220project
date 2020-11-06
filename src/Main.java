abcf122eb04a3a4ff0d1cc9fcefd5963179e877a
DEMEMBERTM19 <DEMEMBERTM19@5CD9253756.GCC.edu>
2020-11-06 13:48:43 -0500

    Get outplayed

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Bye :)");
-
-		System.out.println("hi there teamates");
-//>>>>>>> branch 'main' of https://github.com/RedLaunchTech/220project
-
+		System.out.println("I win");
 	}
 
 }
