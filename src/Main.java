585fa45e2f9321004119258d3e7088fdb8b205f1
rttru <rttru@10.20.104.174>
2020-11-18 19:30:00 -0700

    added a print statement to say poop in main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void main (String[] args) {
 		Game newGame;
 		newGame = new Game();
-		newGame.gameLoop();	
+		newGame.gameLoop();
+		System.out.println("poop");
 	}	
 }
